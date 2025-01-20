a0d8c6e88ff0910c9c34a8e789a2c46ac5e966bf
Lauro Silva <lauroclara1@gmail.com>
2025-01-20 13:32:08 -0300

    innitialcommit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+const users = []
+
+app.post("/users", (req, res) => {
+    const body = req.body
+    users.push(body)
+    res.status(201).send("Usuário criado com sucesso!");
+});
+
+app.get("/users", (req, res) =>{
+    res.send({message: "Essa são os users", users})
+}
+)
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
